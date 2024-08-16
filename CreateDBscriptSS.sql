891dcb3d5ef743901d20e7193bfd5e70dea78a54
Miguel Covarrubias <macs04@gmail.com>
2024-08-16 13:40:43 -0600

    first commit

---
+++ CreateDBscriptSS.sql
@@ -0,0 +1,7 @@
+ CREATE TABLE persona (
+    id INT PRIMARY KEY,
+    compania NVARCHAR(50) NOT NULL,
+    contacto NVARCHAR(50) NOT NULL,
+    correo NVARCHAR(50) NOT NULL,
+    telefono BIGINT NOT NULL
+);
